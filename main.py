610ba4f31b1b8e51b2e89a39f46e332564dea2dc
stergiosbamp <bampakis.stergios@gmail.com>
2019-04-29 11:58:03 +0300

    Renamed parser

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     classes_edges_tuple = es_parser.get_classes_vertices()
     methods_edges_tuple = es_parser.get_methods_vertices()
 
-    graph_operations.write_gexf(classes_edges_tuple, "/home/stergios/Desktop/class_graph.gexf")
-    graph_operations.write_gexf(methods_edges_tuple, "/home/stergios/Desktop/methods_graph.gexf")
+    # graph_operations.write_gexf(classes_edges_tuple, "/home/stergios/Desktop/class_graph.gexf")
+    # graph_operations.write_gexf(methods_edges_tuple, "/home/stergios/Desktop/methods_graph.gexf")
 
-    # graph_operations.write_gexf(classes_edges_tuple, "/home/stergios/Desktop/directed_classes.gexf")
-    # graph_operations.write_gexf(methods_edges_tuple, "/home/stergios/Desktop/directed_methods.gexf")
+    graph_operations.write_gexf(classes_edges_tuple, "/home/stergios/Desktop/directed_classes.gexf")
+    graph_operations.write_gexf(methods_edges_tuple, "/home/stergios/Desktop/directed_methods.gexf")
 
